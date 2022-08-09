226378fc66fe90f833c9aa158525c0f38e158c46
ewaldstrasser <32096824+ewaldstrasser@users.noreply.github.com>
2022-08-09 15:07:20 +0200

    Add files via upload
    This is the submission of Ewald.

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Ewald Strasser__
+
+# Ewald Strasser - Scientist
+
+# *Ever since I got a little understanding of statistics I wanted to work with data in a hands on fashion. I think data scientists exactly do that and therefore I want to become one.
+
+# ### The next line will set the variable x to 5
+
+# In[5]:
+
+
+x=5
+10-x
+
+
+# 1. one
+# 2. two 
+# 3. three
+# - four
+# * five
+# |six | seven |
+# |:----:|:---:|
+# | eight | nine |
+# 
+# ~ten~
+
+# In[ ]:
+
+
+
+
